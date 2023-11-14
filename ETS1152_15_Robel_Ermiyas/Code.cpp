63bdfd34b64b4d8ef58fbbc924f1c78324e01f53
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2023-11-14 22:24:03 +0300

    Create Code.cpp

---
+++ ETS1152_15_Robel_Ermiyas/Code.cpp
@@ -0,0 +1,14 @@
+1.
+
+#include <iostream>
+using namespace std;
+int main() {
+    float weight, hight, BMI;
+    cout <<"weight";
+    cin >>weight;
+    cout <<"hight";
+    cin >>hight;
+    BMI = weight/(hight*hight);
+    cout <<"BMI="<<BMI;
+    return 0;
+}
