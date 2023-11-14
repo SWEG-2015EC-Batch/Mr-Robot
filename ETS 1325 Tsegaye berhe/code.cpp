240cd42de3a6f620257a20578c626c937c9cae26
tsegaye-berhe <149230383+tsegaye-berhe@users.noreply.github.com>
2023-11-15 01:08:16 +0300

    Create code.cpp

---
+++ ETS 1325 Tsegaye berhe/code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+double height , weight , Bmi;
+cout<< " This program is design to calculate Bmi"<< endl;
+cout << "please enter your weight and height"<< endl;
+cout << "weight"<< endl;
+cin>> weight;
+cout<< "height"<< endl;
+cin >> height;
+Bmi= weight/ (height*height);
+cout << "BMI"<< Bmi;
+return 0;
+}
