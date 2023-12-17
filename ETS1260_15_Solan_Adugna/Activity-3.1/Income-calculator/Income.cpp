229740d4a97eb817bd7dc479ceafa2b31a7fe44c
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-17 00:35:37 -0800

    Update Income.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Income-calculator/Income.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Income-calculator/Income.cpp
@@ -1,1 +1,42 @@
+//This program is used to calculate an Net-Pay of an employee
+#include <iostream>
+using namespace std;         
+
+int main(){
+    double NetPay, grossSalary ,workedHours ,overTimeBonusRate,
+          pension, incomeTax, overTimePayment;
+
+
+    cout <<"           Net-salary calculator" << endl;
+
+    cout <<"\nPlease enter your gross salary: ";
+    cin >> grossSalary;
+    pension = 0.7*grossSalary;
+    if (grossSalary < 200)
+        incomeTax = 0;
+     else if (grossSalary >= 200 && grossSalary < 600)
+        incomeTax = 0.1*grossSalary;
+       else if (grossSalary >= 600 && grossSalary < 1200)
+        incomeTax = 0.15*grossSalary;
+    else if (grossSalary >=1200 && grossSalary < 2000)
+        incomeTax = 0.2*grossSalary;
+    else if (grossSalary >= 2000 && grossSalary < 3500)
+        incomeTax = 0.25*grossSalary;
+    else
+        incomeTax = 0.3*grossSalary;
+
+    cout <<"Please enter the worked hours: ";
+   cin >> workedHours;
+    if (workedHours > 40) {
+        cout <<"Please enter over-time bonus rate /hour: ";
+        cin >> overTimeBonusRate;
+        overTimePayment=(overTimeBonusRate * (workedHours-40));
+        NetPay=grossSalary - pension-incomeTax + overTimePayment;
+        }
+        else
+        NetPay=grossSalary - pension-incomeTax;
+
+    cout <<"The Net-Pay of an employee;  " << NetPay;
+    return 0;
+}
 
