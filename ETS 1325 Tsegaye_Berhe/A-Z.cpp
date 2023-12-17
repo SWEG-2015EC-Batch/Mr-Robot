765a530f742df8325048ef897d85dda5e1f78e85
tsegaye-berhe <149230383+tsegaye-berhe@users.noreply.github.com>
2023-12-17 21:36:37 +0300

    Add files via upload

---
+++ ETS 1325 Tsegaye_Berhe/A-Z.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout << "A B C D E F " << endl;
+cout << "G H I J K L" << endl;
+cout << "M N O P Q R" << endl;
+cout << "S T U V W X " << endl;
+return 0;
+}
