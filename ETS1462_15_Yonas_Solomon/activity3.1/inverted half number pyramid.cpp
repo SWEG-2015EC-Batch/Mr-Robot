1b5a53a42b803275708b2ca3b10797303d23373f
yona354 <149327146+yona354@users.noreply.github.com>
2023-12-17 06:38:16 -0800

    Create inverted half number pyramid.cpp

---
+++ ETS1462_15_Yonas_Solomon/activity3.1/inverted half number pyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int i, j, rows;
+   cout << "Enter the number of rows\n";
+   cin >> rows;
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         cout << j;
+      }
+      cout <<endl;
+   }
+   return 0;
+}
