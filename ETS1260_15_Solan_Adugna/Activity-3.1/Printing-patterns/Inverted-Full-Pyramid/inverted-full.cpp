2c903a21cfd1e29aafaeaa6be729de03c732b620
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-17 12:10:41 -0800

    Update inverted-full.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Inverted-Full-Pyramid/inverted-full.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Inverted-Full-Pyramid/inverted-full.cpp
@@ -1,1 +1,25 @@
+// C++ Program to print the inverted full pyramid
+#include <iostream>
+using namespace std;
 
+int main()
+{
+       int n;
+       cout<<"Enter size of row: ";
+       cin>>n;
+    for (int i = 0; i < n; i++) {
+        int space = i;
+        for (int j = 0; j < 2 * n - i - 1; j++) {
+
+            if (space) {
+                cout << "  ";
+                space--;
+            }
+            else {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+return 0;
+}
