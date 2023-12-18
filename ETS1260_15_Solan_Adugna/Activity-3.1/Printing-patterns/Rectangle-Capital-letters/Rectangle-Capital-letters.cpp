4b07eb5ec49dd7701603c5d6d5d6a31b10068628
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-18 08:22:30 -0800

    Update Rectangle-Capital-letters.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle-Capital-letters/Rectangle-Capital-letters.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle-Capital-letters/Rectangle-Capital-letters.cpp
@@ -1,1 +1,19 @@
-
+//Program that print Rectangle with capital letter
+#include <iostream>
+using namespace std;
+int main(){
+    int colomn,row;
+    cout<< "enter the row: ";
+    cin >> row;
+    cout << "enter the colomn: ";
+    cin >> colomn;
+    int k(1);
+    for(int i=1; row>=i; i++){
+     for(int j=1; colomn>=j; j++){
+        cout << char(64+k)<< " "; //
+        k++;
+     }
+     cout << endl;
+    }
+    return 0;
+}
