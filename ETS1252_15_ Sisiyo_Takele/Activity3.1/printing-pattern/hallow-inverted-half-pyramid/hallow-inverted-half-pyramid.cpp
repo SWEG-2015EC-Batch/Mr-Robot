06b69caa80f0709f3e97dd7eeac9abd4125e1377
sisiyotakele <149423407+sisiyotakele@users.noreply.github.com>
2023-12-18 17:27:15 +0300

    Create hallow-inverted-half-pyramid.cpp

---
+++ ETS1252_15_ Sisiyo_Takele/Activity3.1/printing-pattern/hallow-inverted-half-pyramid/hallow-inverted-half-pyramid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+int side, i=1,j,k=1;
+cout<< "This program will make hollow inverted half pyramid ";
+cout << "Enter the side ";
+cin >> side;
+j=side;
+// row loop
+while(side >= i){
+// for first row
+if (i==1){
+for(k=1;j>=k;){
+    cout << "*  ";
+    k++;
+    }
+cout<<endl;
+j--;
+i++;
+continue;
+}
+// other rows
+for(k=1;j>=k;){
+if(k==1 || k==j){
+cout <<"*  ";
+k++;
+continue;
+}
+cout << "   ";
+k++;
+}
+j--; // decrease size of loop
+cout<< endl; 
+i++; // for next row 
+}    
+}
