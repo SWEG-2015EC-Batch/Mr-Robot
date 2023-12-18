93e241c92cf8c411593cc68aaca2728ea1d9e50f
tsegaye-berhe <149230383+tsegaye-berhe@users.noreply.github.com>
2023-12-18 13:15:18 +0300

    Add files via upload

---
+++ ETS 1325 Tsegaye_Berhe/Activity_3.1/stars rectangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+for (int i=1; i<=5;i++){
+cout <<"*"<< " ";
+}
+cout <<endl;
+for (int i=1; i<=5;i++){
+cout <<"*"<< " ";}
+cout << endl;
+for (int i=1; i<=5;i++){
+cout <<"*"<< " ";}
+cout << endl;
+for (int i=1; i<=5;i++){
+cout <<"*"<< " ";}
+
+    return 0;
+}
