722ab5a8128e167d82de4ff889097e2f9021618f
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-18 07:20:58 -0800

    Update Rectangle- small-lette.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle- small-letter/Rectangle- small-lette.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle- small-letter/Rectangle- small-lette.cpp
@@ -1,1 +1,19 @@
-
+// program that print rectangle small letter
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+int main(){
+char letter;
+int row,colomn;
+cout<< "Enter the raw: ";
+cin>> row;
+cout<< "Enter the colomn: ";
+cin>> colomn;
+    for(int i=1; i<=row;i++){
+        for(int j=1;j<=colomn;j++){
+            cout << char(96+j)<< "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
