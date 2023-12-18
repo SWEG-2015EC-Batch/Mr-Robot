d27251e6a8df6c763b0e8a1c3d28fae29499711e
sisiyotakele <149423407+sisiyotakele@users.noreply.github.com>
2023-12-18 20:10:38 +0300

    Create capial-letter-rectangle.cpp

---
+++ ETS1252_15_ Sisiyo_Takele/Activity3.1/printing-pattern/capital-letter-rectangle/capial-letter-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int colomn,row;
+    cout<< "enter the row: ";
+    cin >> row;
+    cout << "enter the colomn: ";
+    cin >> colomn;
+    int k(1);
+    for(int i=1; row>=i; i++){
+     for(int j=1; colomn>=j; j++){
+        cout << char(64+k)<< " "; //
+        k++;
+     }
+     cout << endl;
+    }
+    return 0;
+}
