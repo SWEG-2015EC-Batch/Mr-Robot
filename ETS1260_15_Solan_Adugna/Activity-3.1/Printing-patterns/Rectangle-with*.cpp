fc4b509496fb7b162795efa57266dd5daf1fe195
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-18 06:08:53 -0800

    Update Rectangle-with*.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle-with*.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rectangle-with*.cpp
@@ -1,1 +1,23 @@
+//  program that print rectangle with *
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    // Initialize variables
+    int row, column;
+
+                cout << "Please enter the number of columns for the box: ";
+                cin >> column;
+                cout << "Please enter the number of rows for the box: ";
+                cin >> row;
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < column; j++) {
+                        cout << "* ";
+                    }
+                    cout << endl;
+                }
+
+      return 0;
+
+}
