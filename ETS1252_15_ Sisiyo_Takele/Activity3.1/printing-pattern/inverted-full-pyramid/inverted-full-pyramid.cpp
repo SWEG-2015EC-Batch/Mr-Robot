de3e14429ed84cb6055be5d839cb4979c58ffff1
sisiyotakele <149423407+sisiyotakele@users.noreply.github.com>
2023-12-18 17:41:59 +0300

    Create inverted-full-pyramid.cpp

---
+++ ETS1252_15_ Sisiyo_Takele/Activity3.1/printing-pattern/inverted-full-pyramid/inverted-full-pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       int n;
+       cout<<"Enter size of row: ";
+       cin>>n;
+    for (int i = 0; i < n; i++) {
+        int space = i;
+        for (int j = 0; j < 2 * n - i - 1; j++) {
+
+            if (space) {
+                cout << "  ";
+                space--;
+            }
+            else {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+return 0;
+}
