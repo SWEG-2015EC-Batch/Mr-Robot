a3497ee034ae5f9279fff6d61d0993da30fa175a
sisiyotakele <149423407+sisiyotakele@users.noreply.github.com>
2023-12-18 20:19:36 +0300

    Create capital-letter-half-pyramid.cpp

---
+++ ETS1252_15_ Sisiyo_Takele/Activity3.1/printing-pattern/capital-letter-half-pyramid/capital-letter-half-pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int side,j(1);
+cout << "enter the side: ";
+cin>> side;
+for(int i=1;i<=side;i++){
+    for(int k=1;k<=j;k++){
+        cout << char(64+k)<< "  ";
+    }
+    j++;
+    cout << endl;
+}
+return 0;
+}
