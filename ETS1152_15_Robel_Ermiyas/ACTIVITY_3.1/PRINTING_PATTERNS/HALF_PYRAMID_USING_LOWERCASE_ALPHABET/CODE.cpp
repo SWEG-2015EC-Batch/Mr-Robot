39e669d9efbce5772d61e5145f013ac105fe4e71
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2023-12-18 08:42:22 +0300

    Update CODE.cpp

--- ETS1152_15_Robel_Ermiyas/ACTIVITY_3.1/PRINTING_PATTERNS/HALF_PYRAMID_USING_LOWERCASE_ALPHABET/CODE.cpp
+++ ETS1152_15_Robel_Ermiyas/ACTIVITY_3.1/PRINTING_PATTERNS/HALF_PYRAMID_USING_LOWERCASE_ALPHABET/CODE.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    for (char i = 'a'; i <= 'e'; ++i) {
-        for (char j = 'a'; j <= i; ++j) {
+    for (char i = 'A'; i <= 'E'; ++i) {
+        for (char j = 'A'; j <= i; ++j) {
             cout << j << " ";
         }
         cout << endl;
