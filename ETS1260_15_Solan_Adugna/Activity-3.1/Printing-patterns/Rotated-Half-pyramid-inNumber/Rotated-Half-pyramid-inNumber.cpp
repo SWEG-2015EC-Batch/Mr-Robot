b961a038bcd060e6e0d8b2c73673c7142a085561
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-18 08:53:07 -0800

    Update Rotated-Half-pyramid-inNumber.cpp

--- ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rotated-Half-pyramid-inNumber/Rotated-Half-pyramid-inNumber.cpp
+++ ETS1260_15_Solan_Adugna/Activity-3.1/Printing-patterns/Rotated-Half-pyramid-inNumber/Rotated-Half-pyramid-inNumber.cpp
@@ -1,1 +1,25 @@
-
+// program to Rotated  half pyramid with number
+#include <iostream>
+using namespace std;
+int main(){
+int side,j(1),k(1),space,spacecheker(1);
+cout<<"Program to construct inverted half triangle\nside: ";
+cin >> side;
+space=side;
+for(int i=1; i<=side;i++){
+    while (space>spacecheker){
+        cout <<"   ";
+        spacecheker++;
+    }
+    spacecheker=1;
+    space--;
+    k=j;
+    while(j>0){
+        cout << j<<"  ";
+        j--;
+    }
+    j=k+1;
+    cout << endl;
+}
+return 0;
+}
