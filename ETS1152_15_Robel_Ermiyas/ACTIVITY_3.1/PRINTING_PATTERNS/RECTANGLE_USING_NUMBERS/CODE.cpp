d58959fc31f9c31658a48feaa12e49a9d563090b
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2023-12-18 07:25:38 +0300

    Create CODE.cpp

---
+++ ETS1152_15_Robel_Ermiyas/ACTIVITY_3.1/PRINTING_PATTERNS/RECTANGLE_USING_NUMBERS/CODE.cpp
@@ -0,0 +1,12 @@
+//A C++ program to print a rectangle using numbers.
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        for (int j = 1; j <= 5; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
