7634a4261553edccb51ece7b690a41798f93fd0a
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-19 03:25:31 -0800

    Update Transmission-Time.cpp

--- ETS1260_15_Solan_Adugna/First-Coding-practical-exercise/Transmission-Time/Transmission-Time.cpp
+++ ETS1260_15_Solan_Adugna/First-Coding-practical-exercise/Transmission-Time/Transmission-Time.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+int main() {
+    int transmissionRate = 960; // characters per second
+    int fileSize = 419430400; // file size in bytes
+
+    double timeInSeconds = fileSize / transmissionRate;
+    double timeInMinutes = timeInSeconds / 60;
+    double timeInHours = timeInMinutes / 60;
+    double timeInDays = timeInHours / 24;
+
+    std::cout << "Time to send the file:" << std::endl;
+    std::cout << "Seconds: " << timeInSeconds << std::endl;
+    std::cout << "Minutes: " << timeInMinutes << std::endl;
+    std::cout << "Hours: " << timeInHours << std::endl;
+    std::cout << "Days: " << timeInDays << std::endl;
+
+    return 0;
+}
