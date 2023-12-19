115631c936ea68a99ab5a4609030b1d938996b8b
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2023-12-19 11:38:08 -0800

    Update Code.cpp

--- ETS1260_15_Solan_Adugna/Activity-4.1/Code.cpp
+++ ETS1260_15_Solan_Adugna/Activity-4.1/Code.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+     cout<<"Enter size of numbers you want to enter"<<endl;
+    cin >> n;
+    double sum = 0;
+    double data;
+    double squaredSum = 0, dataSum = 0;
+
+    for (int i = 0; i < n;++i){
+        cout<<"Enter data: ";
+        cin >> data;
+        dataSum += data;
+        squaredSum += pow(data, 2);
+    }
+    double mean = dataSum / n;
+    double sd = sqrt((squaredSum - (pow(dataSum, 2)) / n) / (n - 1));
+    cout << "Mean: " << mean << endl;
+    cout << "Standard Deviation: " << sd << endl;
+}
 
