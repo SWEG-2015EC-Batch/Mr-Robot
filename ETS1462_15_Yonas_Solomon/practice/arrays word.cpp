543a85c7f6e097c59ecfd06d75e47ddbed7c84ce
yona354 <149327146+yona354@users.noreply.github.com>
2023-12-25 04:54:24 -0800

    Create arrays word.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/arrays word.cpp
@@ -0,0 +1,14 @@
+//Write array declarations, including initializers, for the following:
+//a. A list of 10 integer voltages: 89, 75, 82, 93, 78, 95, 81, 88, 77, and 82.
+//b. A list of five double-precision slopes: 11.62, 13.98, 18.45, 12.68, and 14.76.
+#include<iostream>
+
+using namespace std;
+int mani() {
+    int voltages[10] = {89, 75, 82, 93, 78, 95, 81, 88, 77, 82};
+    double precision_slopes[5] = {11.62, 13.98, 18.45, 12.68, 14.76};
+    cout << precision_slopes;
+    string weak_end []={"cool"};//using c++
+    string weak_ends [4]={"cool"};//using c+9
+    return 0;
+}
