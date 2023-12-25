15394179861b4e4e330f7fad632ad00f283a9d5d
yona354 <149327146+yona354@users.noreply.github.com>
2023-12-25 05:16:47 -0800

    Create differentiate 1D & 2D.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/differentiate 1D & 2D.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int mani() {
+    string weak_end ={"cool "};//using c++ 0ne D//string also can be char then use ''
+    string weakends [] ={"cool "};//using c++ two D
+    
+    char weakendss [5]={'c','o','o','l'};//using c 1 D
+  //  char weakendss [5][]={'c','o','o','l'};//using c 1 D
+    return 0;
+    //
+}
