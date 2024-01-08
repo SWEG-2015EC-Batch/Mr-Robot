ebee7627961422ddbaa4a613c153c57298b47f49
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-08 08:13:19 -0800

    Create string & getline.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/BC/string & getline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //different between if else and while
+    string name;
+    cout<<"enter a name :";
+    getline(cin,name);
+   if(name=="yonas solomon"){
+         cout<<"the paswoarrd is u!";
+    }
+   else cout<<"try again!";
+}
