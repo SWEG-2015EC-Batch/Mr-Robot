f16ca8991f4853f964605b387c11e7188b9d04ee
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-08 07:49:20 -0800

    Create math.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/BC/math.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5;
+    int y=7;
+    int z;
+    int w;
+    w =min(x,y);
+    z=max(x,y);
+    cout<<w<<"\n";
+    cout<<z<<"\n";
+    //using the math header floor()
+    //abs()
+    //ceil()        means increas to upper but round to the upper
+    //round()   just round
+    //pow()
+    //sqrt()
+    // in hypotnous formula to find the hypotunos sqrt(pow(a,2)+pow(b,2))
+}
