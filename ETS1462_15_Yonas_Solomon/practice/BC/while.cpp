97ada225a32f9e1c9697cc30a4f8f9e54efb048c
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-08 08:34:37 -0800

    Create while.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/BC/while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //different between if else and while
+  //while loop repeat the condition in infinite times if the condition is true
+   int x;
+    cout<<"enter a number :";
+    cin>>x;
+   while(x==12){
+         cout<<"i love you (* ^ *)";
+    }
+  cout<<"try again!";
+    return 0;
+}
