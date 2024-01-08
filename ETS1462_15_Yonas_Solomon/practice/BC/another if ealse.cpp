e5cb603df2e23e7361f51c8d87ac60104910d315
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-08 08:01:45 -0800

    Create another if ealse.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/BC/another if ealse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter test result:";
+    cin>>x;
+   // if(x<10){
+     //   cout<<"not bad!\n";
+   // }
+  //  cout<<"good!";
+    //to write if statement in another way
+    x<10 ? cout<<"not bad!\n" : cout<<"good!";
+}
