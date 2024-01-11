9fc16d935537959b2afbe440dd1f886b151eecdc
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-11 08:12:30 -0800

    Update Q-2.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-2.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-2.cpp
@@ -1,1 +1,26 @@
+/*A program to input eight integer numbers into an array named temp. As each number is input, add the number into a total. 
+After all numbers are input,display the number and their average.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int temp[8];
+    float total = 0;
+          // Input numbers and add to total
+    for(int i=0; i<8; i++) {
+        cout << "Enter number " << i+1 << ": ";
+        cin >> temp[i];
+        total += temp[i];
+    }
+           // Calculate average
+    float average = total / 8;
+
+    // Display total and average
+    cout << "Total: " << total << endl;
+    cout << "Average: " << average << endl;
+
+    return 0;
+}
 
