984c992b55c16fd3fab83ac2cd1b58e6dd4d346a
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-11 08:21:34 -0800

    Update Q-1.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-1.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-1.cpp
@@ -1,1 +1,22 @@
+/*A program to declare a 4-by-5 array of integers and initialize with the data;
+ 16, 22, 99, 4, 18, -258, 4, 101, 5, 98, 105, 6, 15, 2, 45, 33, 88, 72, 16, and 3.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4][5] = {{16, 22, 99, 4, 18},{-258, 4, 101, 5, 98},{105, 6, 15, 2, 45},{33, 88, 72, 16, 3}};
+
+    // Print the array
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<5; j++) {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
