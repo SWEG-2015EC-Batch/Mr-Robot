fbf509b1018c694e37a13e408bcec6af419ed47f
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-11 08:21:53 -0800

    Update Q-1.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-1.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/1D-Array/Q-1.cpp
@@ -1,4 +1,4 @@
-/*A program to declare a 4-by-5 array of integers and initialize with the data;
+/*A program to declare a 4-by-5 array of integers and initialize with the data:
  16, 22, 99, 4, 18, -258, 4, 101, 5, 98, 105, 6, 15, 2, 45, 33, 88, 72, 16, and 3.
  */
 
