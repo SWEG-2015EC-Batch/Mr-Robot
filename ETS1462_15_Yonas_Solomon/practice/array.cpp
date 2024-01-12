b8b4b902ebb0a691f9afa3dad71ddacecf88cb7b
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-11 23:22:08 -0800

    Create array.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/array.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string cars[]/*size of data*/={"lamborgini","toyota","chavrolate"};
+    cout<<cars;//the out put gives us the addres of the string car not out put all the cars name
+}
