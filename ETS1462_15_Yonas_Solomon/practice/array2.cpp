0aa07585f4da6bfa15cfe087f99fa861b87d462e
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-11 23:24:59 -0800

    Create array2.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/array2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string cars[]/*size of data*/={"lamborgini","toyota","chavrolate"};
+    cout<<cars[0];// only when we include the size itgives us the car name  
+}
