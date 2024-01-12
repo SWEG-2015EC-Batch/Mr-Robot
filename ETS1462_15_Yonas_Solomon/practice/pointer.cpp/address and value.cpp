bd2915a15177ef1a02cab6bbdaa141c064d60469
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-12 04:16:04 -0800

    Create address and value.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/pointer.cpp/address and value.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<stdio.h>
+int main(){
+    int age = 10;
+    printf("the adres:%p\n ",&age);//address %p    &age
+    printf("value of age:%d\n ",age);//value %d    age
+    return 0; }
