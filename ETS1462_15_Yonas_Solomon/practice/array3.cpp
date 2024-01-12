8ed419997acaa22fe7c4a894571a5bff119df00c
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-11 23:30:20 -0800

    Create array3.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/array3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string cars[]/*size of data*/={"lamborgini","toyota","chavrolate"};
+   // cout<<cars[0];
+   //what if i want to display all the cars name by using for loop
+   for(int i=0;i<size(cars);i++){
+      // cout<<cars[i]<<"\t";//display all the cars in horizontal
+       cout<<cars[i]<<"\n";
+   }
+}
