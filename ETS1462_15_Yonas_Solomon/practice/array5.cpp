6245330204058600301949c34460337bdea20f72
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-12 01:19:27 -0800

    Create array5.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/array5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /* int number[19] = {1,2,3,4,
+                    5,6,7};
+    cout<<number[19];//the rest filed with zero iff its in the array size range*/
+   int number[]={1,2,3,4,5,6};
+   cout<<number[4];//in array initialization no need to put array size its useless instate it save our memory wastage by not put the size
+    return 0;
+}
