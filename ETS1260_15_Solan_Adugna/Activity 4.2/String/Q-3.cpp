f607be5d323fd5bcd8326f03299afde310419d69
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 10:03:43 -0800

    Update Q-3.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-3.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-3.cpp
@@ -1,1 +1,24 @@
+// A program to remove non-alphabet character from string
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    string result = "";
+
+    for (char character : input) {
+        if (isalpha(character)) {
+            result += character;
+        }
+    }
+
+    cout << "String after removing non-alphabet characters: " << result << endl;
+
+    return 0;
+}
 
