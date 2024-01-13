1e4eef031ea7dc34be6e613326be538a07e8274c
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2024-01-13 08:24:45 +0300

    Create Q4.cpp

---
+++ ETS1152_15_Robel_Ermiyas/ACTIVITY_4.2/STRING/Q4.cpp
@@ -0,0 +1,27 @@
+// A C++ program takes a line of text as input and tokenizes it using space characters as delimiters.
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+int main() {
+    string line;
+    cout << "Enter a line of text: ";
+    getline(std::cin, line);
+
+    vector<std::string> tokens;
+    istringstream iss(line);
+    string token;
+
+    while (getline(iss, token, ' ')) {
+        tokens.push_back(token);
+    }
+
+    cout << "Tokens: ";
+    for (const auto& t : tokens) {
+        cout << t << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
