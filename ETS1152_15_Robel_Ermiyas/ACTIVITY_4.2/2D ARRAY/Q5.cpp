770e3767aebbe69057370315163b49d5f99daac8
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2024-01-13 10:50:36 +0300

    Update Q5.cpp

--- ETS1152_15_Robel_Ermiyas/ACTIVITY_4.2/2D ARRAY/Q5.cpp
+++ ETS1152_15_Robel_Ermiyas/ACTIVITY_4.2/2D ARRAY/Q5.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+#include <vector>
+
+int countFriendPairs(std::vector<std::vector<bool>>& friends) {
+ int count = 0;
+ int n = friends.size();
+
+ for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+        if (friends[i][j]) {
+         count++;
+ }
+}
+ }
+
+ return count;
+}
+
+int main() {
+ std::vector<std::vector<bool>> friends = {
+ {false, true, false, true},
+ {true, false, true, false},
+ {false, true, false, true},
+ {true, false, true, false}
+ };
+
+ int pairs = countFriendPairs(friends);
+
+ std::cout << "Number of friend pairs: " << pairs << std::endl;
+
+ return 0;
+}
