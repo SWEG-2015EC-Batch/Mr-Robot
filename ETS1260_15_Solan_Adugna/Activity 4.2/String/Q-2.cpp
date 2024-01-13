af7cedd954aa8d8b47145b12bc896aea7a638f23
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 10:00:47 -0800

    Update Q-2.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-2.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-2.cpp
@@ -1,1 +1,29 @@
+// program to check either the word is palindrome or not using loop
 
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a word: ";
+    cin >> input;
+
+    int length = input.length();
+    bool isPalindrome = true;
+
+    for (int i = 0; i < length / 2; ++i) {
+        if (tolower(input[i]) != tolower(input[length - 1 - i])) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome) {
+        cout << input << " is a palindrome." << endl;
+    } else {
+        cout << input << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
