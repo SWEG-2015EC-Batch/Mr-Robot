48a5dbd6897d865f536941063680784ca667fe63
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-12 21:23:03 -0800

    Create array cycle input.cpp

---
+++ ETS1462_15_Yonas_Solomon/practice/array cycle input.cpp
@@ -0,0 +1,17 @@
+//for loop can be used to cycle through the array for interactive data input
+#include<iostream>
+using namespace std;
+int main(){
+    double nums [][3]={{1,2,3},{4.5,6,7},{4,5,6}};
+    for(int i=0;i<size(nums);i++){
+        string numi;
+        cout<<"enter raws number:";
+        cin>>numi[i];
+    }
+    for(int i=0;i<size(nums);i++) {
+        string numj;
+        cout << "enter columns number:";
+        cin >> numj[i];
+    }
+    return 0;
+}
