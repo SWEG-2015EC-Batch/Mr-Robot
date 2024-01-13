e656dc96e1b4fd1f582a909afcabc7ba6624bce1
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 10:09:50 -0800

    Update Q-5.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-5.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-5.cpp
@@ -1,1 +1,37 @@
+//A program that find the frequency of vowel, consonant, digit and special character
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int vowels = 0, consonants = 0, digits = 0, specials = 0;
+
+    for (char character : input) {
+        if (isalpha(character)) {
+            char lowerChar = tolower(character);
+            if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u')
+                {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        } else if (isdigit(character)) {
+            digits++;
+        } else {
+            specials++;
+        }
+    }
+
+    cout << "Frequency of vowels: " << vowels << endl;
+    cout << "Frequency of consonants: " << consonants << endl;
+    cout << "Frequency of digits: " << digits << endl;
+    cout << "Frequency of special characters: " << specials << endl;
+
+    return 0;
+}
 
