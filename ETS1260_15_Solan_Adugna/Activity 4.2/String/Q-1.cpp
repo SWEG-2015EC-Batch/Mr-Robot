728eacb3abbd0952cd91aeacfebe433618941508
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 09:57:57 -0800

    Update Q-1.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-1.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-1.cpp
@@ -1,1 +1,33 @@
+/*A program that read a string and count number of vowels and consonants alphabet
+present in a string.
+*/
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    int vowelsCount = 0;
+    int consonantsCount = 0;
+
+    for (char character : input) {
+        if (isalpha(character)) {
+            char lowerChar = tolower(character);
+            if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
+                vowelsCount++;
+            } else {
+                consonantsCount++;
+            }
+        }
+    }
+
+    cout << "Number of vowels: " << vowelsCount << endl;
+    cout << "Number of consonants: " << consonantsCount << endl;
+
+    return 0;
+}
 
