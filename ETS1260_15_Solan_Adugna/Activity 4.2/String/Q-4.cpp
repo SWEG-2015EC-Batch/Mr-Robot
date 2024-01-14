60a974555ef3a8c3301e406527c25322762b633f
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:17:25 -0800

    Update Q-4.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-4.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-4.cpp
@@ -1,1 +1,34 @@
+/*A application that inputs a line of text, and tokenizes it. Use space characters as 
+delimiters.
+*/
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string inputText;
+
+    // Input a line of text
+    cout << "Enter a line of text: ";
+    getline(cin, inputText);
+
+    // Tokenize the input using space characters as delimiters
+    istringstream iss(inputText);
+    vector<string> tokens;
+
+    string token;
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+
+    // Output the tokens
+    cout << "Tokens: ";
+    for (const auto& t : tokens) {
+        cout << t << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
