bbee0441d784a8bef1ea13d083dc9651b8e7dfe1
tsegaye-berhe <149230383+tsegaye-berhe@users.noreply.github.com>
2024-01-14 10:27:38 +0300

    Add files via upload

---
+++ ETS 1325 Tsegaye_Berhe/Activity 2.1/miles 3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+double gallons , miles ;
+cout <<" this program is design to calculate how"<< endl;
+cout <<" many miles can your automobile travel per gallons" << endl;
+    cout <<"please enter amount of gallons left on"<< endl;
+    cout <<"on your automobile"<<endl;
+     cout <<"gallons = ";
+     cin >> gallons ;
+     miles= 30 *gallons;
+     cout << "your car will stop  after " << miles << " miles ";
+    return 0;
+} 
