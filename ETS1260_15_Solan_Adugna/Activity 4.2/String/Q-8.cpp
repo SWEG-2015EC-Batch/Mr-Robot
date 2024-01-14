6667b0c9007453d1f4b0edd5e25cdf023757af8c
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:01:20 -0800

    Update Q-8.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-8.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-8.cpp
@@ -1,1 +1,26 @@
+/*An application that inputs a line of text and outputs the text twice, once in all uppercase
+and once in all lowercase letters.
+*/
+#include <iostream>
+#include <cctype>
+using namespace std;
 
+int main() {
+    string input;
+        cout << "Enter a line of text: ";
+    getline(cin, input);
+
+    cout << "Uppercase: ";
+    for (char character : input) {
+        cout << char(toupper(character));
+    }
+    cout << endl;
+
+    cout << "Lowercase: ";
+    for (char character : input) {
+        cout << char(tolower(character));
+    }
+    cout << endl;
+
+    return 0;
+}
