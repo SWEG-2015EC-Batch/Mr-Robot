cd38ac1709ba0c59fe0bd259d271c1c757e9c90c
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:59:24 -0800

    Update Q-4.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/2D-Array/Q-4.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/2D-Array/Q-4.cpp
@@ -1,1 +1,33 @@
+/*A C++ program, to read 3*4 2D-array, find and print the sum of even array elements. The 
+program should also print even array elements and ‘*’ instead of odd numbers in tabular form
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int array[3][4];
+cout << "Enter elements for the 3x4 array:\n";
+    for (int i = 0; i <3; ++i) {
+        for (int j = 0; j <4; ++j) {
+            cout << "Element at position (" << i + 1 << ", " << j + 1 << "): ";
+            cin >> array[i][j];
+        }
+    }
+int sumEven = 0;
+    cout << "\nEven elements with '*' for odd numbers:\n";
+    for (int i = 0; i <3; ++i) {
+        for (int j = 0; j <4; ++j) {
+            if (array[i][j] % 2 == 0) {
+                sumEven += array[i][j];
+                cout << array[i][j] << "\t";
+            } else {
+                cout << "*\t";
+            }
+        }
+        cout << "\n";
+    }
+
+    cout << "\nSum of even elements: " << sumEven << "\n";
+     return 0;
+}
 
