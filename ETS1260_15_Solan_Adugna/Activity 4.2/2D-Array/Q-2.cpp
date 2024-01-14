c0193015d5d5fc0087f5cf5d1bfaaa9b916667c1
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:56:34 -0800

    Update Q-2.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/2D-Array/Q-2.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/2D-Array/Q-2.cpp
@@ -1,1 +1,31 @@
-
+/*A a C++ program to that accepts a two-dimensional array and displays the multiplication
+of row elements and the sum of column elements.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+   int sample[3][4];//5 can be change accord to data
+   for(int i=0;i<3;++i){
+     for(int j=0;j<4;++j){
+    cout<<"enter the "<<i+1<<" row and "<<j+1<<" coloumn number: ";
+    cin>>sample[i][j];
+     }
+     }
+     cout << "Multiplication of row elements:\n";
+    for (int i = 0; i <3; ++i) {
+        int rowProduct = 1;
+        for (int j = 0; j <4; ++j) {
+            rowProduct *= sample[i][j];
+        }
+        cout << "Row " << i + 1 << ": " << rowProduct << "\n";
+    }
+     cout << "Sum of column elements:\n";
+    for (int j = 0; j <4; ++j) {
+        int colSum = 0;
+        for (int i = 0; i <3; ++i) {
+            colSum += sample[i][j];
+        }
+        cout << "Column " << j + 1 << ": " << colSum << "\n";
+    }
+     return 0;
+}
