b32c4156e7f77710aba9b31506ada53c8809d706
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:05:08 -0800

    Update Qu-11.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Qu-11.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Qu-11.cpp
@@ -1,1 +1,23 @@
+/* A a program that asks the user for its first, middle and last name and then prints the
+user’s initials on-screen by printing the first character of each name.
+*/
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstName, middleName, lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your middle name: ";
+    cin >> middleName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "Your initials full name letter are: " << firstName[0]<<", "<< middleName[0]<<", " << lastName[0]<<", " << endl;
+
+    return 0;
+}
