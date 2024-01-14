fbf0d70a5643a65fe6569a1a410b974fd7fd23bc
yona354 <149327146+yona354@users.noreply.github.com>
2024-01-13 23:14:45 -0800

    Create Q2.cpp

---
+++ ETS1462_15_Yonas_Solomon/activity4.2/1D array/Q2.cpp
@@ -0,0 +1,20 @@
+// A C++ program that display the number it self and their average.
+#include <iostream>
+using namespace std;
+int main() {
+    int temp[8];
+    int total = 0;
+
+    for (int i = 0; i < 8; i++) {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> temp[i];
+        total += temp[i];
+    }
+
+    double average = static_cast<double>(total) / 8;
+
+    cout << "Total: " << total << endl;
+    cout << "Average: " << average << endl;
+
+    return 0;
+}
