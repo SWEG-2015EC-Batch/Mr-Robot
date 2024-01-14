795c7e315b5153625ed3ede63fa842489424f499
Solan Adugna <149063873+solanadugna@users.noreply.github.com>
2024-01-13 21:02:40 -0800

    Update Q-9.cpp

--- ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-9.cpp
+++ ETS1260_15_Solan_Adugna/Activity 4.2/String/Q-9.cpp
@@ -1,1 +1,24 @@
+/*A program to store and print the names of your two favorite television programs.
+Initialize one of the strings (assign it the first program’s name) at the time you declare the
+array. Initialize the second value in the body of the program with the strcpy() function.
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char favoriteProgram1[] = "Breaking Bad";
+    char favoriteProgram2[50];
+
+    strcpy(favoriteProgram2, "Stranger Things");
+
+    // Printing the names of favorite television programs
+    cout << "My two favorite TV programs are: " << endl;
+    cout << "1. " << favoriteProgram1 << endl;
+    cout << "2. " << favoriteProgram2 << endl;
+
+    return 0;
+}
+
 
