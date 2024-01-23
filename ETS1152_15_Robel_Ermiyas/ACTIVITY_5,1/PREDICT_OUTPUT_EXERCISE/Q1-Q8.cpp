d5fac4479e25c30282f69cee92394cfaa4460191
Robelermiyas <149297975+Robelermiyas@users.noreply.github.com>
2024-01-23 23:08:22 +0300

    Create Q1-Q8.cpp

---
+++ ETS1152_15_Robel_Ermiyas/ACTIVITY_5,1/PREDICT_OUTPUT_EXERCISE/Q1-Q8.cpp
@@ -0,0 +1,51 @@
+// quetion number 1 output:
+
+
+// question number 2 output:
+ 58 58 58
+
+// question number 3 output:
+k =4
+x=0x61ff08
+y=0x61ff04
+k = 14
+k = 28
+
+  // question number 4 output:
+        i             j              ptr                            pptr
+4       5             10            0x37129                      0x1fc825
+5       5             10            0x37129                      0x1fc825
+6       3             10            0x37129                      0x1fc825
+7       7             10            0x37129                      0x1fc825
+8       7             10            0x5863a                      0x1fc825
+9       7             9             0x37129                      0x1fc825
+10      7             9             0x37129                      0x1fc825
+11     -2             9             0x37129                      0x1fc825
+
+// question number 5 output:
+  5 0x61ff04
+6422272 0x61ff08
+6422320 0x61ff0c
+3682304 0x61ff10
+3682305 0x61ff14
+3682306 0x61ff14
+3682306 0x61ff14
+
+  // question number 6 out put:
+653.9654e+03378.99964e-039
+
+  // question number 7:
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
